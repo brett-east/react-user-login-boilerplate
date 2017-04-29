6eb791c9a78db8dbd99ef104d4bbede359da7edd
Brett East <brett@sproutback.com>
2017-04-29 16:02:56 +1000

    Upgrade node_modules and to webpack v2

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ var path = require('path');
 
 module.exports = {
   entry: [
-    'script!jquery/dist/jquery.min.js',
-    'script!foundation-sites/dist/js/foundation.min.js',
+    'script-loader!jquery/dist/jquery.min.js',
+    'script-loader!foundation-sites/dist/js/foundation.min.js',
     './app/app.jsx'
   ],
   externals: {
@@ -21,28 +21,41 @@ module.exports = {
     filename: './public/bundle.js'
   },
   resolve: {
-    root: __dirname,
+    modules: [
+      __dirname,
+      'node_modules',
+      './app/components'
+    ],
     alias: {
       Main: 'app/components/Main.jsx',
       applicationStyles: 'app/styles/app.scss'
     },
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   module: {
-    loaders: [
+    rules: [
       {
+        test: /\.jsx?$/,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015', 'stage-0']
         },
-        test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
+      },{
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader",
+          options: {
+            includePaths: [
+              path.resolve(__dirname, './node_modules/foundation-sites/scss')
+            ]
+          }
+        }]
       }
-    ]
-  },
-  sassLoader: {
-    includePaths: [
-      path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
   devtool: 'eval-source-map'
