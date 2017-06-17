bc5b895d0fa6384a19c31eb4208a4c371954ef9b
Brett East <brett@sproutback.com>
2017-06-17 14:43:26 +1000

    Style 404 page

--- app/components/NotFound.js
+++ app/components/NotFound.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 
 export default () => {
   return (
-    <div className="boxed-view">
-      <div className="boxed-view__box">
-        <h1>Page Not Found</h1>
-        <p>Hmm, we're unable to find that page.</p>
-        <Link className="button button--link" to="/">HEAD HOME</Link>
+    <div className="panel-view">
+      <div className="panel-view__panel">
+        <div className="panel-view__column">
+          <h1>Page Not Found</h1>
+          <p>Hmm, we're unable to find that page.</p>
+          <Link className="button button--link" to="/">HEAD HOME</Link>
+      </div>
       </div>
     </div>
   );
